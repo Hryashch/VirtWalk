795f23651782bc5f4fe3c8b79cafdc9456fffb1e
Hryashch <hryashchser@gmail.com>
2024-05-02 23:44:35 +0300

    adaptive ui for both mobile and desktop

--- lib/placesearch.dart
+++ lib/placesearch.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'dart:io';
+// import 'dart:io';
 
 // import 'package:google_maps_apis/places.dart';
 //final gplaces = GoogleMapsPlaces(apiKey: apiKey);
